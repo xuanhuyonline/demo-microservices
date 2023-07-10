a1647c531febfc9c8dcd5e14de5177d84a841c37
xuanhuyonline <lexuanhuy2k1@gmail.com>
2023-07-10 17:45:41 +0700

    create a Spring cloud config server

---
+++ config-server/src/test/java/s3/corp/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package s3.corp.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
